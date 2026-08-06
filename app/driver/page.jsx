a02b8480853ee6f0052b9d18a6ec2855ef78853c
secgelbora-coder <secgelbora@gmail.com>
2026-08-06 06:49:54 -0400

    Create page.jsx

---
+++ app/driver/page.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+import DriverView from '@/components/DriverView'; // Bileşenin bulunduğu konuma göre yolu düzenleyin
+
+export default function DriverPage() {
+  return <DriverView />;
+}

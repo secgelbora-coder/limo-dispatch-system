0d207336ca8e66ae7e53f2eeb7344c5d4fd73458
secgelbora-coder <secgelbora@gmail.com>
2026-08-06 06:28:38 -0400

    Update driver.jsx

--- frontend/driver.jsx
+++ frontend/driver.jsx
@@ -3,12 +3,12 @@ import DriverView from './DriverView';
 export default function DriverPage() {
   // Örnek canlı veri şablonu
   const sampleTrip = {
-    tripId: "41711",
-    companyName: "SLADE SERVICES",
-    passengerName: "Ed Butera",
-    passengerPhone: "(561) 706-1976",
+    tripId: "00001",
+    companyName: "Blue Printell",
+    passengerName: "Hakan Alp",
+    passengerPhone: "(561) 601-8721",
     passengerCount: 1,
-    pickupAddress: "1050 NW 1st Ave Boca Raton FL 33432",
+    pickupAddress: "1041 NW 2nd Ave, Fort Lauderdale, FL 33311",
     dropoffAddress: "FLL - Fort Lauderdale-Hollywood International Airport",
     pickupTime: "9:30 AM",
     dropoffTime: "10:30 AM",
